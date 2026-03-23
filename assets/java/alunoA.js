aa98d08f4f30c88ba887e0973906203c1254f48e
Priscila Nunes <priscilavespoli92@gmail.com>
2026-03-23 12:42:12 +0100

    Parte A ok

---
+++ assets/java/alunoA.js
@@ -0,0 +1,7 @@
+// TRABAJO DEL ALUMNO A
+// Instrucción: Crea un arreglo con 5 palabras que formen una frase, pero desordenadas.
+
+const piezas = ["vale", "maña", "que", "Más", "fuerza"];
+
+console.log("Arreglo original desordenado:");
+console.log(piezas);
