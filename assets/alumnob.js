8f6da8691a675eda536870d967dd21331b33cce9
Melina <melina.dmec@gmail.com>
2026-03-23 13:04:06 +0100

    alumno b

---
+++ assets/alumnob.js
@@ -0,0 +1,9 @@
+const mensajeOrdenado =
+piezas [3]+ " " +
+piezas [0]+ " " +
+piezas [1]+ " " +
+piezas [2]+ " " +
+piezas [4]+ " " ;
+
+console.log ("mas vale maña que fuerza");
+console.log(mensajeOrdenado);
